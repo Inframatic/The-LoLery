d147f89d0c4c3162c65a023ddf5dc76bb3d3621b
inframatic <isteffy@hotmail.com>
2013-09-02 13:41:38 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheLolery::Application.config.secret_key_base = 'dfc28361094e9dc9f8fac64a7c201b39a74729bd2d5d6dcc2871d91899268d9578fe5ca66d18599fff869cd7db259555ed0b0f0ae5c06dc039f3f313f41273c5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TheLolery::Application.config.session_store :cookie_store, key: '_the_lolery_session'
