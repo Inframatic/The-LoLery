7fa72769a0c73d849da890b6338affafd459751f
inframatic <isteffy@hotmail.com>
2013-09-03 12:41:04 -0400

    fixed some spacing issues which intersected with the _header

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ TheLolery::Application.routes.draw do
   get "users/new"
   root 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
