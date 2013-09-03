3991d434d7b531ec77dec62f59d86b5bd1843172
inframatic <isteffy@hotmail.com>
2013-09-03 12:27:57 -0400

    updated the header to have sign in, sign out and profile/setting dropdown menu

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,11 +7,16 @@ module SessionsHelper
     self.current_user = user
   end
 
+  def signed_in?
+    !current_user.nil?
+  end
+
   def current_user=(user)
     @current_user = user
   end
 
-   def current_user
-    @current_user     # Useless! Don't use this line.
+  def current_user
+    remember_token = User.encrypt(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
   end
 end
