69436d1905c2820782c24f92b5af0d359ffc9053
inframatic <isteffy@hotmail.com>
2013-09-03 16:30:59 -0400

    added the index.html and def index to project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+
   def new
     @user = User.new
+  end
+
+  def index
   end
 
   def show
@@ -35,19 +39,22 @@ class UsersController < ApplicationController
 
 private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 
-    # Before filters
+  # Before filters
 
-    def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
     end
+  end
 
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
-    end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 end
