a4ad8b5221ff864223793c634fdc2d963f85bf2a
inframatic <isteffy@hotmail.com>
2013-09-06 08:26:43 -0400

    added nokogiri to controller

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -1,3 +1,10 @@
 class ChampionsController < ApplicationController
-	
+	  def index
+      require 'nokogiri'
+      doc = Nokogiri::XML(open("http://sports.yahoo.com/top/rss.xml"))
+
+      @links = doc.xpath('//item').map do |i|
+      {'title' => i.xpath('title'), 'link' => i.xpath('link'), 'description' => i.xpath('description')}
+      end
+  end
 end
