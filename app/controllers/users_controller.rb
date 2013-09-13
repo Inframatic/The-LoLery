f9f4faea27f69eceb8b0727f178d8c8dd380ce87
inframatic <isteffy@hotmail.com>
2013-09-13 11:38:52 -0400

    specified comments controller to only have index create and destroy, added a method to make sure the user is signed in before the controller is used

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
     @videos = @user.videos.paginate(page: params[:page])
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 TheLolery::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :comments, only: [:create, :destroy]
+  resources :videos
+  resources :champions
+
   root 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
@@ -9,10 +13,10 @@ TheLolery::Application.routes.draw do
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   
-  resources :champions
+  
   match '/champions', to: 'champions#index', via: 'get'
 
-  resources :videos
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
