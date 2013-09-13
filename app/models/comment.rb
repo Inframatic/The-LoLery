97a398a8e6dc16bb73d4668ee7b8575a95797800
inframatic <isteffy@hotmail.com>
2013-09-13 11:30:51 -0400

    added _comments.html.erb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ActiveRecord::Base
+	belongs_to :user
+	default_scope -> { order('created_at DESC') }
+	validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :comments
 	before_save { email.downcase! }
 	before_create :create_remember_token
   validates :name,  presence: true, length: { maximum: 50 }
