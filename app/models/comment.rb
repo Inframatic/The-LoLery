b536a8029d9efd65134098904b84ec6034d726bb
inframatic <isteffy@hotmail.com>
2013-09-14 11:33:12 -0400

    fixed the partials error by adding a loop in user/show for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
-	belongs_to :user
-	default_scope -> { order('created_at DESC') }
-	validates :user_id, presence: true
+  belongs_to :user
+  default_scope -> { order('created_at DESC') }
+  validates :content, presence: true, length: { maximum: 140 }
+  validates :user_id, presence: true
 end
