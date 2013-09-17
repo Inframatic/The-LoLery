86662e639c1ab5906fdfb1b6ac4e45531e1fdeba
inframatic <isteffy@hotmail.com>
2013-09-17 12:13:33 -0400

    users can now post videos from youtube

--- app/helpers/videos_helpers.rb
+++ app/helpers/videos_helpers.rb
@@ -2,8 +2,4 @@ module VideosHelper
 	def format_date(time)
 		time.in_time_zone("Eastern Time (US & Canada)").strftime("%B %e %Y - %l:%M %p")
 	end
-
-	def code
-		self.video.split('/').last if self.video
-	end
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -2,5 +2,9 @@ class Video < ActiveRecord::Base
 	has_many :comments
 	belongs_to :user
 	default_scope -> { order('created_at DESC') }
-	validates :video, presence: true, length: { maximum: 70 }
+	validates :video, presence: true, length: { maximum: 420 }
+
+	def code
+	  self.video.try(:split, '/').try(:last) || ''
+	end
 end
